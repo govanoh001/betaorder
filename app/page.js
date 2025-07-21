f842a4dfbe30f33ddeb7eb9265ca375f0d110738
govanoh001 <uzomonoble@gmail.com>
2025-07-21 12:16:05 +0100

    first commit

--- app/page.js
+++ app/page.js
@@ -1,8 +1,112 @@
-import { Main } from "next/document";
-import Image from "next/image";
 
-export default function Home() {
+export default function HomePage() {
   return (
-    <main>wwelcome to our landing page</main>
-  );
-}
+    <main> 
+      <div className="flex gap-25 w-full h-[100px] bg-white items-center sticky top-0">
+      <hi className="italic font-extrabold text-6xl text-red-700 px-2">KFC</hi>
+      <div className="tracking-tighter uppercase font-bold flex gap-20 text-2xl hover:text-red-700 ">
+        <p className="hover:border-b-2 border-red-700">Company</p>
+        <p className="hover:border-b-2 border-red-700">Brand Impact</p>
+        <p className="hover:border-b-2 border-red-700">Careers</p>
+        <p className="hover:border-b-2 border-red-700">Newsroom</p>
+      </div>
+      <div className="flex items-center gap-3">
+      <input
+       type="text"
+       placeholder="SEARCH ITEMS"
+       className="h-[50px] w-[200px] rounded-full border border-2 px-4"/>
+       <img 
+       src="/search.png"
+       alt="search"
+       width={30}
+       height={30}
+       className="w-[30px] h-[30px] "/>
+       </div>
+      </div>
+       <div className="bg-[url('/bg2.jpg')] bg-cover bg-center h-screen w-full flex grid grid-cols-4 justify-between gap-43 items-center px-3">
+        <div className="">
+        <h1 className="text-white text-8xl font-extrabold ">30K+</h1>
+        <p className="text-lg font-bold text-white" >RESTURANT</p>
+        </div>
+        <div>
+        <h1 className="text-white text-8xl font-extrabold">1M</h1>
+        <p className="text-lg font-bold text-white">TEAM MEMBERS</p>
+        </div>
+        <div>
+        <h1 className="text-white text-8xl font-extrabold">150</h1>
+        <p className="text-lg font-bold text-white">COUNTRIES WITH KFC</p>
+        </div>
+        <div>
+        <h1 className="text-white text-8xl font-extrabold">3.5</h1>
+        <p className="text-lg font-bold text-white">BEFORE NEXT KFC IS BUILT</p>
+        </div>
+       </div>
+       <div className="w-full h-[35vh] bg-red-700 ">
+       <div className="flex gap-50 px-8 items-center justify-center">
+        <p className="text-white font-semibold mt-10">KFC, a subsidiary of Yum! Brands, has <br/>
+         been serving craveworthy, finger lickin’<br/>
+          good food since 1952.</p>
+          <p className="text-white font-semibold mt-10">It all started with our chef and expert<br/> 
+          tastemaker, Colonel Harland Sanders,<br/>
+           who went on a relentless mission to <br/>
+           crack and create our iconic taste.</p>
+           <p className="text-white font-semibold mt-10">Today, we have the widest global<br/>
+            footprint of any quick service <br/>
+            restaurant brand and are one of the<br/>
+             world’s fastest growing retail brands.</p>
+          </div>
+       </div>
+       <div className="bg-[url('/bg3.jpg')] bg-cover bg-center h-[50vh] w-full py-20 px-20 italics">
+         <h1 className="font-extrabold text-4xl text-white uppercase">Chilli Lime Dunked <br/>
+         Burger</h1>
+         <h2 className="font-bold text-3xl text-white mt-3">SOUTH AFRICA</h2>
+          <p className="font-semibold text-white mt-3">A spicy, tangy burger featuring a Zinger fillet dunked in a <br/>
+          Chilli Lime sauce, topped with cheese, fresh Rainbow slaw <br/>
+          and Colonel Dressing, served on a Brioche bun.</p>
+       </div>
+       <div className="flex ">
+        <img
+        src="/kfcc.jpg"
+        alt="kfcimage"
+        width={450}
+        height={450}
+        className="w-[450px] h-[450px]"/>
+        <div>
+        <img
+        src="/kfccc.jpg"
+        alt="kfcimage"
+        width={450}
+        height={450}
+        className="w-[450px] h-[450px]"/>
+        </div>
+        <img
+        src="/bgkfc.webp"
+        alt="kfcimage"
+        width={445}
+        height={450}
+        className="w-[440px] h-[450px]"/>
+       </div>
+       <div className="bg-black h-[50vh] w-full">
+        <div className="flex flex-col items-center justify-center py-20">
+          <h1 className="text-red-700 text-2xl font-bold hover:text-white">CONTACT</h1>
+          <p className="text-white font-semibold mt-4 ">We’re in the chicken business, but <br/>
+          we’re also in the people business.</p>
+          <button className="bg-red-700 h-[30px] w-[150px] rounded-full text-white font-semibold hover:bg-white hover:text-red-700 mt-4">Contact Us</button>
+        </div>
+        <div className="flex gap-15 ml-60">
+        <p className="text-white font-semibold ">Copyright © 2025 Yum! Ltd. All rights reserved.</p>
+        <p className="text-white font-semibold border-b-1 border-white">Private Policies</p> 
+        <p className="text-white font-semibold border-b-1 border-white ">Term of Use</p> 
+        <p className="text-white font-semibold border-b-1 border-white ">SiteMap</p>  
+        </div>
+       </div>
+    </main>
+   
+  )
+    }
+
+
+
+
+
+
